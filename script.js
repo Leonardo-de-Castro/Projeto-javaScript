fe385d698d08a9eefdfaafd3c08c6e4754d27487
Leonardo de Castro <127856638+Leonardo-de-Castro@users.noreply.github.com>
2023-06-07 19:37:59 -0300

    atualização dos comandos Scripts

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'imagens/Manhã.png'
+        document.body.style.backgroundColor = '#FDEFC6'
+    }
+    else  if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'imagens/Tarde.png'
+        document.body.style.backgroundColor = '#C37F74'
+    }
+    else {
+        //BOA NOITE!
+        img.src = 'imagens/Noite.png'
+        document.body.style.backgroundColor = 
+    }
+}
+
